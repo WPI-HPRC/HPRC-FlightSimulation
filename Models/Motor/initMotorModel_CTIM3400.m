df8fc65a414b8063dd0135aa055becd56620d231
Dan Pearson <dpear0915@gmail.com>
2024-09-16 16:42:48 -0400

    Added mass curve

--- Models/Motor/initMotorModel_CTIM3400.m
+++ Models/Motor/initMotorModel_CTIM3400.m
@@ -1,4 +1,4 @@
-function ModelData = initMotorModel_CTIM3400(plotThrustCurve)
+function ModelData = initMotorModel_CTIM3400(motorPlots)
 %% Motor Model - CTIM3400
 % Author: Daniel Pearson (djpearson@wpi.edu)
 % Version: 9.16.2024
@@ -9,21 +9,20 @@ function ModelData = initMotorModel_CTIM3400(plotThrustCurve)
 %   ModelData - Struct containing motor constants and thrust curve
 
 %% Default Parameter Val
-if ~exist('plotThrustCurve', 'var')
-    plotThrustCurve = false;
+if ~exist('motorPlots', 'var')
+    motorPlots = false;
 end
 
 %% Constants
-consts.totalImpulse = 9990;      % [N-s] Total Impulse
-consts.avgThrust    = 3414;      % [N] Average Thrust
-consts.maxThrust    = 3898;      % [N] Max Thrust
-consts.burnTime     = 2.92;      % [s] Burn Time
-consts.m_i          = 8.108;     % [kg] Initial Mass
-consts.m_f          = 3.342;     % [kg] Final Mass
-consts.m_p          = consts.m_i - consts.m_f; % [kg] Propellant Mass
-consts.diameter     = 98e-3;     % [m] Motor Diameter
-consts.I_sp         = 213.74;    % [s] Specific Impulse
-
+consts.totalImpulse = 9990;                    % [N-s] Total Impulse
+consts.avgThrust    = 3414;                    % [N] Average Thrust
+consts.maxThrust    = 3898;                    % [N] Max Thrust
+consts.burnTime     = 2.92;                    % [s] Burn Time
+consts.m_i          = 8.108;                   % [kg] Initial Mass
+consts.m_p          = 4.452;                   % [kg] Propellant Mass
+consts.m_f          = consts.m_i - consts.m_p; % [kg] Final Mass
+consts.diameter     = 98e-3;                   % [m] Motor Diameter
+consts.I_sp         = 213.74;                  % [s] Specific Impulse
 
 %% Thrust Curve
 thrustCurveData = load('M3400_ThrustCurve.mat');
@@ -38,11 +37,18 @@ thrustCurveData = load('M3400_ThrustCurve.mat');
 % Makima interpolate into anonymous function
 thrustPolar = @(t) interp1(thrustCurveData.time, thrustCurveData.thrust, t, 'makima');
 
+%% Prop Mass Curve
+propMassCurveData = load('M3400_MassCurve.mat');
+
+massPolar = @(t) interp1(propMassCurveData.time, propMassCurveData.mass, t, 'spline');
 
 ModelData.thrustPolar = thrustPolar;
 ModelData.consts = consts;
 
-if(plotThrustCurve)
+%% Mass Flow Rate Curve
+mDotCurve = diff(massPolar, t);
+
+if(motorPlots)
     figure('Name', 'Thrust Curve');
     scatter(thrustCurveData.time, thrustCurveData.thrust, 'o', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b');
     hold on;
@@ -53,8 +59,17 @@ if(plotThrustCurve)
     ylabel('Time (s)');
     legend('ThrustPTS', 'Function');
     grid on;
+
+    figure('Name', 'Mass Curve');
+    scatter(propMassCurveData.time, propMassCurveData.mass, 'o', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b');
+    hold on;
+    fplot(massPolar, [min(propMassCurveData.time), max(propMassCurveData.time)], 'r');
+    hold off;
+    title('CTI-M3400 Thrust Curve');
+    ylabel('Mass (g)');
+    xlabel('Time (s)');
+    legend('ThrustPTS', 'Function');
+    grid on;
 end
-
-
 
 end
